fe39af308bb326be9eb21c7800071c85631bfc2a
crwohlers <101900065+crwohlers@users.noreply.github.com>
2022-11-19 23:31:25 -0700

    begin migrating python to js

--- indextablecreator.py
+++ indextablecreator.py
@@ -6,29 +6,7 @@ dsource = open("data.csv", "r", encoding='utf-8-sig')
 replacing = False
 
 for line in source:
-    
-    if line == "<!--MARK-->\n":
-        if not replacing:
-            dest.write(line)
-            first = True
-            for dline in dsource:
-                if first:
-                    x = 0
-                    bld = "<tr>"
-                    dline.replace("\n", "")
-                    for head in dline.split(","):
-                        bld += "<th onclick=\"sortTable(" + str(x) + ")\">" + head + "</th>"
-                        x+=1
-                    
-                    dest.write(bld + "</tr>\n")
-                    first = False
-                else:
-                    dline = "<tr><td>" + dline.replace(",", "</td><td>").replace("\n", "") + "</td></tr>\n"
-                    dest.write(dline)
-        replacing = not replacing
-
-    if not replacing:
-        dest.write(line)
+    dest.write(line)
 
 source.close()
 dest.close()
