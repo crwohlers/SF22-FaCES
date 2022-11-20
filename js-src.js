b49130db83e8dd931d54a88a506e768f4c0aeb41
crwohlers <101900065+crwohlers@users.noreply.github.com>
2022-11-20 00:38:16 -0700

    table gen v1

--- js-src.js
+++ js-src.js
@@ -1,5 +1,25 @@
 function createTable(data){
-    console.log(data);
+  table = document.getElementById("myTable2");
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+
+  let keys = Object.keys(data[0]);
+
+  for (let key of keys) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+
+  for (let element of data) {
+    let row = table.insertRow();
+    for (let key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
 }
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
