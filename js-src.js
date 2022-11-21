fae90e539bf144e6055b201f4f65e074ac421004
crwohlers <101900065+crwohlers@users.noreply.github.com>
2022-11-21 11:57:53 -0700

    Update js-src.js

--- js-src.js
+++ js-src.js
@@ -93,7 +93,7 @@ function parseData(){
                 const eles = lines[x].split(/(?!\B\"[^"]*),(?![^"]*\"\B)/);
                 let obj = {};
                 for (let index = 0; index < headers.length; index++) {
-                    console.log(eles[index].replace(/(^"|"$)/, ""));
+                    console.log(eles[index]);
                     obj[headers[index]] = eles[index].replace(/(^"|"$)/g, "");
                 }
 
